42112bef63766f96b49d1fef685e6a93d6c1b0ff
minaung8989889-hash <minaung8989889@gmail.com>
2026-05-23 22:55:39 +0630

    Add files via upload

---
+++ frontend/app.js
@@ -0,0 +1,28 @@
+async function loadData() {
+
+  try {
+
+    const response = await fetch("https://YOUR-RENDER-URL.onrender.com/data");
+
+    const data = await response.json();
+
+    document.getElementById("level").innerText =
+      data.level + " %";
+
+    document.getElementById("temp").innerText =
+      data.temperature + " °C";
+
+    document.getElementById("status").innerText =
+      data.status;
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+
+}
+
+loadData();
+
+setInterval(loadData, 5000);
